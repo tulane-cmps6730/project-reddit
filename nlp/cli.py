53b3661de33b2fa12e32552e72c7288a9129be78
jcollopy-tulane <jcollopy@tulane.edu>
2024-04-27 17:26:42 -0500

    Adding LR

--- nlp/cli.py
+++ nlp/cli.py
@@ -12,8 +12,8 @@ import re
 import requests
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import f1_score, precision_score, recall_score
-from sklearn.naive_bayes import BernoulliNB, LogisticRegression
-
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
 from . import clf_path, config, config_path
 
 @click.group()
