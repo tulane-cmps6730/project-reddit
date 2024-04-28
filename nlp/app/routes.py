5e38d8b02ac007e649cd7e58f7354933e8e40153
jcollopy-tulane <jcollopy@tulane.edu>
2024-04-27 21:15:25 -0500

    Spacing

--- nlp/app/routes.py
+++ nlp/app/routes.py
@@ -14,13 +14,14 @@ labels = ['loss', 'win']
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-	form = MyForm()
+    form = MyForm()
     if form.validate_on_submit():
         input_field = form.input_field.data
-        updated_field = process_text(input_field)  # Ensure consistent spacing
-        X = vec_1.transform([updated_field])  # Correct usage of transformed data
+        updated_field = process_text(input_field)  # Assuming process_text function is defined globally
+        X = vec_1.transform([updated_field])
         pred = bnb.predict(X)[0]
         proba = bnb.predict_proba(X)[0].max()
-        return render_template('myform.html', title='', form=form, prediction=labels[pred], confidence='%.2f' % (proba * 100))
+        # flash(input_field)
+        return render_template('myform.html', title='', form=form, 
+                               prediction=labels[pred], confidence='%.2f' % (proba * 100))
     return render_template('myform.html', title='', form=form, prediction=None, confidence=None)
-	
